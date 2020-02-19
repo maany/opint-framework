e3346d37bb649771ac1ed0930216a01c353ca967
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2020-02-19 12:09:59 +0200

    adds base hdfs provider

--- opint_framework/core/dataproviders/hdfs_provider.py
+++ opint_framework/core/dataproviders/hdfs_provider.py
@@ -1,30 +1,27 @@
-from opint_framework.core.dataproviders.base import BaseLoader
+from .base import BaseLoader
 from pyspark.sql import SparkSession
 import traceback
 
 
 class HDFSLoader(BaseLoader):
     """
-        Base HDFS loader (used to load data from HDFS sources)
+        Base data provider (used to load data from external sources)
     """
 
-    @classmethod
-    def pull_hdfs_json(cls, path, spark):
+    def pull_hdfs_json(self, path, spark):
         try:
             return spark.read.json(path)
         except Exception as e:
             print('Error loading data from', path, e)
             traceback.print_tb(e.__traceback__)
 
-    @classmethod
-    def pull_hdfs_dir(cls, path, spark):
+    def pull_hdfs_dir(self, path, spark):
         try:
             fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
             list_status = fs.listStatus(spark._jvm.org.apache.hadoop.fs.Path(path))
             ret = []
-            # FIXME: pulling works but you can't append datasets like this. Not fixed since method is not yet used.
             for file in [file.getPath().getName() for file in list_status]:
-                ret.append(cls.pull_hdfs_json(path+file, spark))
+                ret.append(self.pull_hdfs_json(path+file, spark))
             return ret
         except Exception as e:
             print('Error listing files for ', path, e)
@@ -37,8 +34,11 @@ class HDFSLoader(BaseLoader):
 
         :return: (data fetched from source)
         """
-        spark = SparkSession.builder.master(kwargs.get('spark_master'))\
-            .appName(kwargs.get('spark_name')).getOrCreate()
+        spark = SparkSession.builder.master(**kwargs.get('spark_master'))\
+            .appName(**kwargs.get('spark_name')).getOrCreate()
         if kwargs.get('type') == 'JSON':
-            return cls.pull_hdfs_json(path=kwargs.get('path'), spark=spark)
+            if kwargs.get('path'):
+                return cls.pull_hdfs_dir(kwargs.get('path'), spark)
+            elif kwargs.get('file'):
+                return cls.pull_hdfs_json(kwargs.get('file'), spark)
         return None
