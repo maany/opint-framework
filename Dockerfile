41a6868389e02bf94e72d77c51b7afd12ee44313
maany <imptodefeat@gmail.com>
2020-03-21 19:36:58 +0100

    docker compose updates

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM python:3.6
 ENV PYTHONUNBUFFERED 1
+ENV DJANGO_SETTINGS_MODULE='opint_framework.conf.settings'
 ENV APP_ROOT /usr/src/app
+ENV PYTHONPATH ${PYTHONPATH}:${APP_ROOT}
 RUN apt-get update && apt-get -y install cron vim python-dev default-libmysqlclient-dev python3-dev
 WORKDIR ${APP_ROOT}
 ADD requirements.txt .
+RUN python -m pip install --upgrade pip
 RUN pip install -r requirements.txt
--- init.sh
+++ init.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 export PYTHONPATH=${APP_ROOT}:${PYTHONPATH}
 mkdir -p ${APP_ROOT}/migrations/core
+mkdir -p ${APP_ROOT}/migrations/data_management
+mkdir -p ${APP_ROOT}/migrations/workload_jobsbuster
+mkdir -p ${APP_ROOT}/migrations/users
+mkdir -p ${APP_ROOT}/migrations/api
 touch ${APP_ROOT}/migrations/core/__init__.py
-python manage.py makemigrations core
+python manage.py makemigrations core data_management workload_jobsbuster users api
 python manage.py migrate
 python manage.py loaddata initial
 echo "0 * * * * root /usr/bin/python3 /code/manage.py rucio_loader_cron >> /code/rucio_loader_output" >> /etc/crontab
 service cron start
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '${ADMIN_PASSWORD}')" | python manage.py shell
 python3 manage.py runserver 0.0.0.0:8080
-
-
-
